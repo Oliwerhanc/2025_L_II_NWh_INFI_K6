e9860a8d4369b8ff253d13c029082c7119ecfddc
Oliwerhanc <kolinirp@gmail.com>
2025-04-29 20:22:47 +0200

    Poprawiono błędy Flake8 2

--- hello_world/__init__.py
+++ hello_world/__init__.py
@@ -1,4 +1,8 @@
-from flask import Flask
-app = Flask(__name__)
+import hello_world.views  # ← import przeniesiony na górę
 
-import hello_world.views  # noqa: C0304
+from hello_world import app
+from hello_world.formater import get_formatted
+from hello_world.formater import SUPPORTED, PLAIN
+from flask import request
+
+# Dodaj pustą linię na końcu pliku
