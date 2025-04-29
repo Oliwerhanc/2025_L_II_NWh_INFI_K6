cc654710d34396e6c9fb176b474825601e9278d8
Oliwerhanc <kolinirp@gmail.com>
2025-04-29 18:12:30 +0200

    Poprawiono błedy i testy JSON

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
+    if request.args.get("output") == "json":
+        return jsonify({"name": "Oliwer"})
     return 'Hello, Oliwer!'
 
 if __name__ == '__main__':
